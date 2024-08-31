0463345811fc930610772b970ba2bd7c130f9898
1U22IT124 <125721583+1U22IT124@users.noreply.github.com>
2024-08-31 05:33:04 +0000

    commit05

--- practical5.java
+++ practical5.java
@@ -1,14 +1,16 @@
 import java.util.HashMap;
- public class practical5 { 
+public class practical5 
+{ 
     public static void main(String[]args)
-     { 
+    { 
         String inputString="HELLO THERE"; 
         HashMap<Character,Integer>charCountMap=countCharacters(inputString); 
         printCharacterCount(charCountMap); 
-    } public static HashMap<Character,Integer>countCharacters(String inputString)
+    } 
+    public static HashMap<Character,Integer>countCharacters(String inputString)
      {
-         HashMap<Character,Integer>charCountMap=new HashMap<>(); 
-         for (int i=0;i< inputString.length();i++)
+        HashMap<Character,Integer>charCountMap=new HashMap<>(); 
+        for (int i=0;i< inputString.length();i++)
           {
              char c=inputString.charAt(i);
               if(Character.isLetter(c)) 
@@ -17,16 +19,21 @@ import java.util.HashMap;
                  if(charCountMap.containsKey(c)) 
                  { 
                     charCountMap.put(c, charCountMap.get(c)+1);
-                 } else 
+                 } 
+                 else 
                  {
                      charCountMap.put(c,1); 
-                    } } } 
-                    return charCountMap;
-                 } public static void printCharacterCount(HashMap<Character,Integer>charCountMap)
-                  {
-                     for(char c:charCountMap.keySet())
-                      { 
-    System.out.println(c + ":"+charCountMap.get(c)); 
-} 
-}
+                    
+                } 
+            } 
+        } 
+        return charCountMap;
+        }
+        public static void printCharacterCount(HashMap<Character,Integer>charCountMap)
+         {
+            for(char c:charCountMap.keySet())
+            { 
+                System.out.println(c + ":"+charCountMap.get(c)); 
+            } 
+          }
 }
